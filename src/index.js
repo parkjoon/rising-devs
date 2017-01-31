8b130c368ac0b3088ff34f05a1b4c900e1a77e71
Joon Park <joon@infinitewebdesign.com>
2017-01-31 17:27:17 -0500

    Refactored CSS files into a separate file in Webpack.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,22 @@ import { Provider } from 'react-redux';
 import App from './components/App.js';
 import RootReducer from './reducers/root';
 
-require('../styles/application.scss');
+import '../styles/vendor/bootstrap/css/bootstrap.min.css';
+import '../styles/vendor/font-awesome/css/font-awesome.min.css';
+import '../styles/vendor/animate/animate.min.css';
+import '../styles/vendor/simple-line-icons/css/simple-line-icons.min.css';
+import '../styles/vendor/owl.carousel/assets/owl.carousel.min.css';
+import '../styles/vendor/owl.carousel/assets/owl.theme.default.min.css';
+import '../styles/vendor/magnific-popup/magnific-popup.min.css';
+import '../styles/css/theme.css';
+import '../styles/css/theme-elements.css';
+import '../styles/css/theme-blog.css';
+import '../styles/css/theme-shop.css';
+import '../styles/vendor/rs-plugin/css/settings.css';
+import '../styles/vendor/rs-plugin/css/layers.css';
+import '../styles/vendor/rs-plugin/css/navigation.css';
+import '../styles/css/skins/skin-corporate-7.css';
+import '../styles/css/custom.css';
 
 const store = createStore(RootReducer);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: 'eval',
@@ -8,7 +9,7 @@ module.exports = {
 		'./src/index.js'
 	],
 	output: {
-		path: path.join(__dirname, 'dist'),
+		path: path.join(__dirname, 'build'), // Might want to change this to 'dist'.
 		filename: 'bundle.js',
 		publicPath: '/build/'
 	},
@@ -20,8 +21,10 @@ module.exports = {
 				include: path.join(__dirname, 'src')
 			},
 			{
-				test: /\.scss$/,
-				loaders: ['style-loader', 'css-loader', 'sass-loader']
+				loader: ExtractTextPlugin.extract({
+					loader: 'css-loader'
+				}),
+				test: /\.(css)$/
 			},
 			{ test: /\.svg$/, loader: 'url-loader?limit=65000&mimetype=image/svg+xml' },
 			{ test: /\.woff$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff' },
@@ -30,5 +33,8 @@ module.exports = {
 			{ test: /\.eot$/, loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject' },
 			{ test: /\.(png|jpg|gif|cur)$/, loader: 'url-loader?limit=8192' }
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css')
+	]
 };
