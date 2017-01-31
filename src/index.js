2babc15bde8dcd130f5cf6196f68540042aa8642
Joon Park <parkjoon@umich.edu>
2017-01-30 20:11:55 -0500

    Installed react-router and redux.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-// Application entrypoint.
-
-// Load the application styles.
-require('../styles/application.scss'); // eslint-disable-line
-
-// Render the top-level React component.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import { browserHistory, Route, Router } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+import App from './components/App.js';
+import RootReducer from './reducers/root';
+
+require('../styles/application.scss');
+
+const store = createStore(RootReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+	<Router history={browserHistory}>
+		<Route path='/' component={App}>
+			{/*<IndexRoute component={FormList} />
+			<Route path="/preview_report_form/report_form_id/:report_form_id" component={FormPreview} />*/}
+		</Route>
+	</Router>
+</Provider>,
+document.getElementById('react-root'));
---
+++ src/reducers/root.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+// export default combineReducers({
+// 	filters,
+// 	filterValues,
+// 	reportForm,
+// 	reportFormList,
+// 	socket,
+// 	userAuthentication
+// });
+
+export default () => {}
